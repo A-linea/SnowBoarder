adcef04071c41f83f9841f205a540b110357d235
Eugene <y.dmitrievskyi@gmail.com>
2023-01-29 00:11:02 +0200

    Added torque amount for snowboarder spinning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D _playerBody;
+
+    [SerializeField] private float torqueAmount = 8f; 
     // Start is called before the first frame update
     void Start()
     {
-        
+       _playerBody =  GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _playerBody.AddTorque(torqueAmount);
+        }
+
+       else if ( Input.GetKey(KeyCode.RightArrow))
+        {
+            _playerBody.AddTorque(-torqueAmount);
+        }
     }
 }
